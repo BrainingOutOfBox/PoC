16e3ce8dfc86d6154d612c534e723b15b27cfc9b
Oli Dias <oli.dias94@protonmail.com>
2018-09-28 11:08:23 +0200

    Projects added

---
+++ Forms.HelloWorld/Forms.HelloWorld/ExampleCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forms.HelloWorld
+{
+    public class ExampleCalculator : ICalculator
+    {
+        public int Add(int augend, int addend)
+        {
+            return augend + addend;
+        }
+
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+    }
+}
---
+++ Forms.HelloWorld/Forms.HelloWorld/ICalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forms.HelloWorld
+{
+    public interface ICalculator
+    {
+        int Add(int augend, int addend);
+        int Subtract(int minuend, int subtrahend);
+    }
+}
